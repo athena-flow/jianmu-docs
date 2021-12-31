59c702ff4034a23dc5a712c98b6c925b134b5a97
liamjung <liamjung@live.com>
2021-12-31 22:21:42 +0800

    feat: cdn wrapper

--- public/refactor.js
+++ public/refactor.js
@@ -1,4 +1,18 @@
-window.onload = () => {
-  document.getElementsByClassName('nav-bar-title')[0].href = '/guide/'
-  document.getElementsByClassName('nav-bar-title')[0].innerHTML = ''
-}
+(function () {
+  if (window.parent === window) {
+    return;
+  }
+
+  let url;
+  // 监听url
+  window.setInterval(() => {
+    if (url && url === window.location.href) {
+      return;
+    }
+
+    url = window.location.href;
+    console.debug('new url: ', url);
+
+    window.parent.postMessage(url, '*');
+  }, 500);
+})();
