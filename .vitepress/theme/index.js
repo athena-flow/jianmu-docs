e2527ae9d31a85e2e8b09bea2a06436678afd00f
liamjung <liamjung@live.com>
2022-01-02 13:54:53 +0800

    refactor: index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -3,12 +3,4 @@ import './custom.css'
 
 export default {
   ...DefaultTheme,
-  enhanceApp({router}) {
-    // 保证页面渲染完成
-    setTimeout(() => {
-      if (router.route.path === '/') {
-        router.go('/guide/')
-      }
-    }, 200);
-  }
 }
